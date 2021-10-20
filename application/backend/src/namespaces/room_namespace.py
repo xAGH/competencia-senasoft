fb1f0007f956c904cd4eea2eac2884ecd0862767
briancastro-bc <bcastro421@misena.edu.co>
2021-10-20 14:39:30 -0500

    Add: connection with rooms. BC

--- application/backend/src/namespaces/room_namespace.py
+++ application/backend/src/namespaces/room_namespace.py
@@ -1,10 +1,12 @@
 from flask import request
-from flask_socketio import send, emit, ConnectionRefusedError, join_room, leave_room
+from flask_socketio import rooms, send, emit, ConnectionRefusedError, join_room, leave_room
 from flask_socketio.namespace import Namespace
+from os import getenv
 
 class RoomNamespace(Namespace):
 
     rooms: dict = {}
+    users_in_room: list = []
 
     def on_connect(self):
         print("Connected")
@@ -13,13 +15,36 @@ class RoomNamespace(Namespace):
         print("Disconnected")
     
     def on_join(self, data):
-        username = data['username']
         room = data['room']
-        self.enter_room(request.sid, room)
-        self.send({
-            "username": username,
-            "message": "has entered the room"
-        }, room=room)
-    
+        username = f"player_{len(self.users_in_room+1)}"
+        try:
+            if len(self.users_in_room) >= int(getenv('ROOMS_LIMIT')):
+                self.emit("room_full", {
+                    "message": "Room is full"
+                }, room=room)
+            self.enter_room(request.sid, room=room)
+            self.users_in_room.append(username)
+            self.emit("user_joined", {
+                "message": "User was joined",
+                "users": self.users_in_room
+            })
+        except ConnectionRefusedError as cr:
+            raise ConnectionRefusedError
+        except Exception as e:
+            raise Exception
+
     def on_leave(self, data):
-        pass
+        room = data['room']
+        username = data['username']
+        try:
+            self.users_in_room.index(username)
+            self.users_in_room.remove(username)
+            self.leave_room(request.sid, room)
+            self.emit("user_leave", {
+                "message": f"User {username} left",
+                "users": self.users_in_room
+            })
+        except ConnectionRefusedError:
+            raise ConnectionRefusedError
+        except Exception:
+            raise Exception
