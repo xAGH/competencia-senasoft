cbdf8f5aa2ab4b2ea0809cf889bbb80ea05aa91a
John James Valencia <jhonjamesillo@gmail.com>
2021-10-20 12:29:43 -0500

    SOCKET: Room messaging and config

--- application/frontend/src/app/app.component.ts
+++ application/frontend/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { SocketioService } from './services/socketio.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bugfinder';
+
+  constructor(private socketSrv: SocketioService) {
+    const data = { username: 'user', room: 'ABCDE' };
+    socketSrv.socket.emit('join', socketSrv.socket.id, data);
+    socketSrv.socket.on('message', (data) => {
+      console.log(data);
+    }
+    );
+  }
+
+  joinRoom(room : string) {
+    if (this.socketSrv.socket.connected) {
+      const data = {'username' : 'USER', room};
+      this.socketSrv.socket.emit('join', data);
+    }
+  }
+
+  connectToSocket() {
+    if (this.socketSrv.socket.disconnected) this.socketSrv.socket.connect();
+  }
+
+  disconnectFromSocket() {
+    if (this.socketSrv.socket.connected) {
+      this.socketSrv.socket.disconnect();
+    }
+  }
 }
