24a8c3ba4802a22f8e5d7703d315f8f2f5abd69f
Alejo <alejito23001@gmail.com>
2021-10-20 11:17:48 -0500

    NEW: Users sigin and signup services

--- application/backend/src/controllers/users_controllers.py
+++ application/backend/src/controllers/users_controllers.py
@@ -2,11 +2,13 @@ from flask import request, make_response, jsonify
 from flask.views import MethodView
 from src.models import Model
 from werkzeug.security import generate_password_hash, check_password_hash
+from src.services.users_service import Users
 
 class UsersSigninController(MethodView):
 
     def __init__(self):
         self.model = Model()
+        self.users = Users()
 
     def post(self):
 
@@ -14,26 +16,12 @@ class UsersSigninController(MethodView):
 
             try:
                 email = request.json['email']
-                verify_email = self.model.fetch_one(f"SELECT email FROM users WHERE email = '{email}'")
+                nickname = request.json['nickname']
+                password = request.json['password']                        
 
-                if verify_email is None or len(verify_email) == 0:
-                    nickname = request.json['nickname']
-                    password = request.json['password']
-                    hash_password = generate_password_hash(password)
-                    self.model.execute_query(f"INSERT INTO users(nickname, email, password) VALUES('{nickname}', '{email}', '{hash_password}')")
-                    
-                    return make_response(jsonify({
-                        "message": "The user was created successfully",
-                        "statuscode": 201
-                    }), 201)
-
-                return make_response(jsonify({
-                    "message": "The user's email is alredy in the database",
-                    "statuscode": 400
-                }), 400)
+                return self.users.signin(email, nickname, password)
 
             except Exception as e:
-                raise e
                 return make_response(jsonify({
                     "message": "Send me an 'email', 'nickname' and 'password' key's.",
                     "statuscode": 400,
@@ -49,6 +37,7 @@ class UsersSigninController(MethodView):
 class UsersSignupController(MethodView):
     def __init__(self):
         self.model = Model()
+        self.users = Users()
 
     def post(self):
         
@@ -57,14 +46,20 @@ class UsersSignupController(MethodView):
             try:
                 email = request.json['email']
                 password = request.json['password']
-                verify_data = self.model.fetch_one("SELECT * FROM users WHERE email = %s" % email)
 
-                if len(verify_data) != 0:
-                    verify_password = check_password_hash(verify_data[1])
+                return self.users.signup(email, password)
 
-            except:
-                pass
+            except Exception as e:
+                return make_response(jsonify({
+                    "message": "Send me an 'email' and a 'password' key's.",
+                    "statuscode": 400,
+                    "error": f"{e}"
+                }), 400)
                 
+        return make_response(jsonify({
+            "message":"Send me a JSON format.",
+            "statuscode":400
+        }), 400)
 
 class UsersAuthController(MethodView):
     pass
