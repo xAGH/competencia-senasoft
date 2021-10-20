50867943a26e694eef6408946d42b362523a7492
briancastro-bc <bcastro421@misena.edu.co>
2021-10-20 10:19:00 -0500

    Merging: alejo's changes. BC

--- application/backend/src/app.py
+++ application/backend/src/app.py
@@ -1,7 +1,9 @@
+from os import getenv
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from src.routes import users, public
+from src.namespaces import *
 
 class Application:
 
@@ -17,11 +19,17 @@ class Application:
 
     @classmethod
     def __settings(cls) -> None:
-        cls.__register_routes()
+        try:
+            cls.__register_routes()
+            cls.app.config.from_mapping(
+                SECRET_KEY = getenv("SECRET_KEY")
+            )
+        except Exception as e:
+            pass
 
     @classmethod
     def __register_namespaces(cls) -> None:
-        pass
+        cls.socketio.on_namespace(RoomNamespace('/room'))
 
     @classmethod
     def __register_blueprints(cls) -> None:
