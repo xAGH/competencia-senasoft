f035a85550b52e73af2dff8e112c40400abc2ac5
John James Valencia <jhonjamesillo@gmail.com>
2021-10-21 09:58:26 -0500

    UPDATE: Home component routing

--- application/frontend/src/app/app.module.ts
+++ application/frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,8 @@ import { SharedModule } from './components/shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
