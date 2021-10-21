a1c7555ae9a9e954afac9293664acf9a535ba9fa
Alejo <alejito23001@gmail.com>
2021-10-21 11:38:46 -0500

    UPDATE: Code comments added in users controllers

--- application/backend/src/controllers/users_controllers.py
+++ application/backend/src/controllers/users_controllers.py
@@ -1,20 +1,36 @@
-from flask import request, make_response, jsonify
+# Importación de librerías.
+from flask import request, make_response, jsonify, Response
 from flask.views import MethodView
 from src.services.users_service import Users
 
+# Este archivo contiene los controladores para los usuarios.
+
 class UsersSignupController(MethodView):
 
-    def __init__(self):
-        self.users = Users()
+    """Clase que controla el registro de los usuarios."""
 
-    def post(self):
+    def __init__(self: object) -> None:
+        """
+        Método constructor.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+            |- Retorno -> None.
+            |- Función -> Instancia un objeto de la clase User para proveer los servicios.
+        """
+        self.users: object = Users()
 
+    def post(self) -> Response:
+        """
+        Método post.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+            |- Retorno -> Response.
+            |- Función -> Valida y verifica los datos que reciba como JSON y método HTTP -> POST para enviarlos al servicio de registro de usuarios.
+        """
         if request.is_json:
 
             try:
-                email = request.json['email']
-                nickname = request.json['nickname']
-                password = request.json['password']                        
+                email: str = request.json['email']
+                nickname: str = request.json['nickname']
+                password: str = request.json['password']                        
 
                 return self.users.signup(email, nickname, password)
 
@@ -32,11 +48,25 @@ class UsersSignupController(MethodView):
 
 
 class UsersLoginController(MethodView):
-    def __init__(self):
-        self.users = Users()
 
-    def post(self):
-        
+    """Clase que controla el login de los usuarios."""
+
+    def __init__(self: object) -> None:
+        """
+        Método constructor.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+            |- Retorno -> None.
+            |- Función -> Instancia un objeto de la clase User para proveer los servicios.
+        """
+        self.users: object = Users()
+
+    def post(self: object) -> Response:
+        """
+        Método post.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+            |- Retorno -> Response.
+            |- Función -> Valida y verifica los datos que reciba como JSON y método HTTP -> POST para enviarlos al servicio de login de usuarios.
+        """
         if request.is_json:
             
             try:
@@ -59,8 +89,24 @@ class UsersLoginController(MethodView):
 
 class UsersAuthController(MethodView):
 
-    def __init__(self):
-        self.users = Users()
+    """Clase que controla la autenticación de usuarios."""
 
-    def get(self, id):
-        return self.users.auth_user(id)
+    def __init__(self: object) -> None:
+        """
+        Método constructor.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+            |- Retorno -> None.
+            |- Función -> Instancia un objeto de la clase User para proveer los servicios.
+        """
+        self.users: object = Users()
+
+    def get(self: object, uid: int) -> Response:
+        """
+        Método get.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+                             uid: int => Id del usuario pasado como parámetro en ruta.
+            |- Retorno -> Response.
+            |- Función -> Atiende a un método HTTP -> GET obteniendo un uid como parametro en ruta y se lo transmite al servicio de
+                          autenticación de usuarios.
+        """
+        return self.users.auth_user(uid)
--- application/backend/src/services/cards_services.py
+++ application/backend/src/services/cards_services.py
@@ -5,22 +5,24 @@ from src.namespaces.room_namespace import RoomNamespace
 
 class Cards():
     
-    """Clase para proveer servicios en cuanto a acciones de las cartas."""
+    """Clase para proveer servicios en cuanto a acciones del juego."""
 
-    def __init__(self) -> None:
+    def __init__(self: object) -> None:
         """
         Método constructor.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
             |- Retorno -> None;
             |- Función -> Inicia una instancia de la clase de Model() para operaciones en base de datos, además, 
                           crea una referencia del diccionario rooms en la clase RoomNamespace.
         """
-        self.model = Model()
-        self.rooms = RoomNamespace.rooms
+        self.model: object = Model()
+        self.rooms: dict = RoomNamespace.rooms
 
-    def get_cards(self) -> tuple:
+    def get_cards(self:object) -> tuple:
 
         """
         Método obtener cartas.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
             |- Retorno -> Tuple(list, list, list) o recibido en tres variables => var1: list, var2: list, var3: list = get_cards()
             |- Función -> Consulta las cartas en la base de datos según su categoria (developer, module, error)
         """
@@ -30,12 +32,13 @@ class Cards():
         errors_cards: list = self.model.fetch_all("SELECT c.id, ca.name AS category_name, c.name as card_name FROM cards c, categories ca WHERE ca.id = c.category AND category = 3", as_dict=True)
         return developers_cards, modules_cards, errors_cards
 
-    def delete_seleted_cards(self, cards: list, selected_card: dict) -> list:
+    def delete_seleted_cards(self: object, cards: list, selected_card: dict) -> list:
         """
         Método eliminar cartas seleccionadas.
-            |- Retorno -> list => Lista de cartas con las seleccionadas ya eliminadas.
-            |- Parámetros -> cards: list => Cartas disponibles.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+                             cards: list => Cartas disponibles.
                              selected_card: dict => Carta seleccionada, pendiente para ser eliminada en la lista de cartas.
+            |- Retorno -> list => Lista de cartas con las seleccionadas ya eliminadas.
             |- Función -> Elimina de una lista de cartas una carta ya seleccionada
         """
         for i in range(len(cards)):
@@ -43,10 +46,11 @@ class Cards():
                 cards.pop(i)
                 return cards
 
-    def select_players_cards(self, cards: list, player: int, room: str) -> tuple:
+    def select_players_cards(self: object, cards: list, player: int, room: str) -> tuple:
         """
         Método seleccionar cartas de jugadores.
-            |- Parámetros -> cards: list => Cartas disponibles para los jugadores.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+                             cards: list => Cartas disponibles para los jugadores.
                              player: int => Posición u orden del jugador.
                              room: str => Código de la sala.
             |- Retorno -> Tuple(list, list) o recibido en dos variables => var1: list, var2: list = select_players_cards(cards)
@@ -60,10 +64,11 @@ class Cards():
             
         return player_cards, cards
 
-    def serve_cards(self, room: str) -> Response:
+    def serve_cards(self:object, room: str) -> Response:
         """
         Metodo repartir cartas.
-            |- Parámetros -> room: str => Código de la sala.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+                             room: str => Código de la sala.
             |- Retorno -> Response => Respuesta enviada en formato json.
             |- Función -> Reparte las a de los jugadores y separa las que quedarán ocultas. Ambas quedan guardadas en el diccionario raiz de rooms..
         """
@@ -89,10 +94,11 @@ class Cards():
             "player4_cards": player4,
         }))
 
-    def add_hidden_cards_to_room(self, cards: dict, room: str) -> None:
+    def add_hidden_cards_to_room(self: object, cards: dict, room: str) -> None:
         """
         Método añadir cartas ocultas a la sala.
-            |- Parámetros -> cards: dict => Cartas elegidas como ocultas. 
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+                             cards: dict => Cartas elegidas como ocultas. 
                              room: str => Codigo de la sala.
             |- Retorno -> None.
             |- Función -> Añade las cartas seleccionadas como ocultas al diccionario de la sala.
@@ -102,10 +108,11 @@ class Cards():
             cards_id.append(card["id"])
         self.rooms[room]["system"]["hidden_cards"] = cards_id
 
-    def question(self, dev_card: int, mod_card: int, error_card: int, room: str) -> Response:
+    def question(self: object, dev_card: int, mod_card: int, error_card: int, room: str) -> Response:
         """
         Método de pregunta.
-            |- Parámetros -> dev_card: int => Id de una carta de desarrollador.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+                             dev_card: int => Id de una carta de desarrollador.
                              mod_card: int => Id de una carta de módulo.
                              error_card: int => Id de una carta de error.
                              room: str -> Código de la sala.
@@ -143,10 +150,11 @@ class Cards():
             "answers": answers
         }))
     
-    def save_discovered_cards(self, room: str, player_index: int, *args: int) -> Response:
+    def save_discovered_cards(self: object, room: str, player_index: int, *args: int) -> Response:
         """
         Método guardar cartas descubiertas.
-            |- Parámetros -> room: str => Código de la sala.
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+                             room: str => Código de la sala.
                              player_index: int => Índice del jugador.
                              *args: int => N argumentos adicionales que corresponden a las cartas a guardar.
             |- Retorno -> Response => Respuesta enviada en formato json.
@@ -160,10 +168,11 @@ class Cards():
             "statuscode":200
         }), 200)
 
-    def accusation(self, room: str, dev_card: int, mod_card: int, error_card: int, player: int) -> Response:
+    def accusation(self: object, room: str, dev_card: int, mod_card: int, error_card: int, player: int) -> Response:
         """
         Método acusación.
-            |- Parámetros -> room: str -> Código de la sala. 
+            |- Parámetros -> self: object => Objeto instanciado que llama al método.
+                             room: str -> Código de la sala. 
                              dev_card: int => Id de una carta de desarrollador.
                              mod_card: int => Id de una carta de módulo.
                              error_card: int => Id de una carta de error.
