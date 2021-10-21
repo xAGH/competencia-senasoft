69fdeef598abe95ac3d0c868466e60c262356d28
John James Valencia <jhonjamesillo@gmail.com>
2021-10-21 07:35:02 -0500

    NEW: Card component

--- application/frontend/src/app/components/shared/card/card.component.ts
+++ application/frontend/src/app/components/shared/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
 
   constructor() { }
+
+  @Input() cardType : string = 'dev';
+  @Input() cardTitle: string = 'CardTitle';
+  @Input() reverse = false;
 
   ngOnInit(): void {
   }
