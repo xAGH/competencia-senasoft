58be1e0dfbe8b53837aebc7cfcb3aaf9549ebbb7
John James Valencia <jhonjamesillo@gmail.com>
2021-10-21 09:13:26 -0500

    UPDATE: Extent class implemented on Page not found

--- application/frontend/src/app/components/public/page-not-found/page-not-found.component.ts
+++ application/frontend/src/app/components/public/page-not-found/page-not-found.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
+  host: {'class': 'full-extent'}
 })
 export class PageNotFoundComponent implements OnInit {
 
