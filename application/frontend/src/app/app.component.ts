30cf3bf304245c51b660971e1631569d12fff638
John James Valencia <jhonjamesillo@gmail.com>
2021-10-21 07:44:14 -0500

    UPDATE: App component reset

--- application/frontend/src/app/app.component.ts
+++ application/frontend/src/app/app.component.ts
@@ -8,36 +8,4 @@ import { SocketioService } from './services/socketio.service';
 })
 export class AppComponent {
   title = 'bugfinder';
-
-  constructor(private socketSrv: SocketioService) {
-    const data = { username: 'user', room: 'ABCDE' };
-    socketSrv.socket.on('message', (data) => {
-      console.log(data);
-    });
-    socketSrv.socket.on('room_full', (data) => {
-      if (data.sid !== socketSrv.socket.id) return;
-      console.log(data);
-    });
-
-    socketSrv.socket.on('user_joined', (data) => {
-      console.log(data)
-    })
-  }
-
-  joinRoom(room : string) {
-    if (this.socketSrv.socket.connected) {
-      const data = {'username' : 'USER', room};
-      this.socketSrv.socket.emit('join', data);
-    }
-  }
-
-  connectToSocket() {
-    if (this.socketSrv.socket.disconnected) this.socketSrv.socket.connect();
-  }
-
-  disconnectFromSocket() {
-    if (this.socketSrv.socket.connected) {
-      this.socketSrv.socket.disconnect();
-    }
-  }
 }
