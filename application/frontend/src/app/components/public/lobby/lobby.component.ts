6b0ab2cc58e01fef6a21b4672f50dd85fe0f087f
John James Valencia <jhonjamesillo@gmail.com>
2021-10-21 16:03:36 -0500

    UPDATE: User lobby connection logic

--- application/frontend/src/app/components/public/lobby/lobby.component.ts
+++ application/frontend/src/app/components/public/lobby/lobby.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PlayerInfo } from 'src/app/interfaces/player-info';
+import { RoomSessionService } from 'src/app/services/room-session.service';
 
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
   styleUrls: ['./lobby.component.scss'],
-  host: {'class': 'full-extent'}
+  host: { class: 'full-extent' },
 })
 export class LobbyComponent implements OnInit {
+  roomCode: string | null = null;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private roomSessionSrv: RoomSessionService
+  ) {}
 
-  roomId : string = 'ABCDE';
+  roomId: string = 'ABCDE';
 
-  playerList = [
-    "AAA",
-    "BBB",
-    "AAA",
-    "BBB",
-  ];
+  playerList: PlayerInfo[] = [];
 
   ngOnInit(): void {
+    this.roomCode = this.route.snapshot.paramMap.get('code');
+    if (this.roomCode) {
+      const playerList = this.roomSessionSrv.info;
+      if (playerList?.users != undefined) this.playerList = playerList?.users;
+    } else {
+      this.router.navigateByUrl('');
+    }
   }
-
 }
