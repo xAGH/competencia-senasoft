1afb43847384eb622f11ced8d009e332a1774037
Alejo <alejito23001@gmail.com>
2021-10-21 12:59:06 -0500

    ADD: Confirmation email

--- application/backend/src/services/email_messages.py
+++ application/backend/src/services/email_messages.py
@@ -9,8 +9,25 @@ class MessageFor():
                              nickname: str => Nickname con el que se identifica el jugador.
                              auth_token: str => Token que contiene la informacion del jugador.
             |- Retorno -> str => Texto de mensaje en formato HTML;
-            |- Función -> Envia un correo desde el email siigobugfinder@outlook.com con el mensaje, asunto y correo entregados
-                          por parámetros.
+            |- Función -> Genera un texto en estructura html.
         """
 
-        return f"""<h1>Works {nickname}.\n<a href="http://localhost:4000/users/auth/{auth_token}">Confirm</a></h1>"""
+        return f"""
+            <div 
+            style="padding: 0%;
+                margin: 0%;
+                width: 100%;
+                height: 100%;
+                background-color: rgb(255, 255, 255);
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;"
+            >
+                <h1>¡Hola {nickname}, gracias por registrarte en SiigoBugFinder!</h1>
+                <h2>Para que tengas acceso a la tabla de puntuaciones y conservar tu nickanme único, <a
+                    style="color: rgb(10, 137, 255);"
+                    href="http://localhost:4000/users/auth/{auth_token}">confirma tu cuenta</a></h2>
+            </div>
+            """
