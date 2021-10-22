0630d1f48f787e766f0fbdb3a982ed697755cf95
John James Valencia <jhonjamesillo@gmail.com>
2021-10-22 10:23:14 -0500

    UPDATE:  Spinner component

--- application/frontend/src/app/components/shared/spinner/spinner.component.ts
+++ application/frontend/src/app/components/shared/spinner/spinner.component.ts
@@ -4,14 +4,16 @@ import { SpinnerService } from 'src/app/services/spinner.service';
 
 @Component({
   selector: 'app-spinner',
-  template: '<div></div>',
+  template: `<div *ngIf="this.spinnerService.isLoading$ | async" class="game-card-container">
+    <img class="game-card" src="assets/cards/Card.png" alt="">
+  </div>`,
   styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent implements OnInit {
 
   public isLoarding$: Subject<boolean> = this.spinnerService.isLoading$;
 
-  constructor(private spinnerService: SpinnerService) { }
+  constructor(public spinnerService: SpinnerService) { }
 
   ngOnInit(): void {
   }
