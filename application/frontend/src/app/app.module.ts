e63281eb5f2c6dd26fde541793ad97d5fbc78a61
John James Valencia <jhonjamesillo@gmail.com>
2021-10-22 10:20:28 -0500

    ADD: App module imports

--- application/frontend/src/app/app.module.ts
+++ application/frontend/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ClipboardModule } from 'ngx-clipboard';
 import { SocketIoModule } from 'ngx-socket-io';
+import { ToastrModule } from 'ngx-toastr';
 import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './components/shared/card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { SharedModule } from './components/shared/shared.module';
 import { ApiInterceptor } from './interceptors/api.interceptor';
 
@@ -25,6 +26,14 @@ import { ApiInterceptor } from './interceptors/api.interceptor';
     }),
     HttpClientModule,
     ClipboardModule,
+    ToastrModule.forRoot({
+      closeButton: false,
+      timeOut: 3000,
+      easeTime: 100,
+      maxOpened: 5,
+      newestOnTop: true,
+    }),
+    BrowserAnimationsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
